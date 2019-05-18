a8cad741db6183d2a7c9df9ae01311cc082918a2
Eric Gustafson <heapsmash@yandex.com>
2019-05-18 08:25:06 -0400

    trying to compile on rbpi

--- dhcp-clos.lisp
+++ dhcp-clos.lisp
@@ -1,47 +1,51 @@
 
 (IN-PACKAGE :DHCP-SERVER)
 (DEFCLASS DHCP NIL
-          ((OP :DOCUMENTATION
-               "Message op code / message type. 1 = BOOTREQUEST, 2 = BOOTREPLY"
-               :ACCESSOR OP :INITARG :OP)
+          ((OP
+             :DOCUMENTATION
+             "Message op code / message type. 1 = BOOTREQUEST, 2 = BOOTREPLY"
+             :ACCESSOR
+             OP
+             :INITARG
+             :OP)
            (HTYPE :DOCUMENTATION
-                  "Hardware address type, see ARP section in \"Assigned Numbers\" RFC; e.g., '1' = 10mb ethernet."
-                  :ACCESSOR HTYPE :INITARG :HTYPE)
+            "Hardware address type, see ARP section in \"Assigned Numbers\" RFC; e.g., '1' = 10mb ethernet."
+            :ACCESSOR HTYPE :INITARG :HTYPE)
            (HLEN :DOCUMENTATION
-                 "Hardware address length (e.g. '6' for 10mb ethernet)."
-                 :ACCESSOR HLEN :INITARG :HLEN)
+            "Hardware address length (e.g. '6' for 10mb ethernet)." :ACCESSOR
+            HLEN :INITARG :HLEN)
            (HOPS :DOCUMENTATION
-                 "Client sets to zero, optionally used by relay-agents when booting via a relay-agent."
-                 :ACCESSOR HOPS :INITARG :HOPS)
+            "Client sets to zero, optionally used by relay-agents when booting via a relay-agent."
+            :ACCESSOR HOPS :INITARG :HOPS)
            (XID :DOCUMENTATION
-                "Transaction ID, a random number chosen by the client, used by the client and server to associate messages and responses between a client and a server."
-                :ACCESSOR XID :INITARG :XID)
+            "Transaction ID, a random number chosen by the client, used by the client and server to associate messages and responses between a client and a server."
+            :ACCESSOR XID :INITARG :XID)
            (SECS :DOCUMENTATION
-                 "Filled in by client, seconds elapsed since client started trying to boot."
-                 :ACCESSOR SECS :INITARG :SECS)
+            "Filled in by client, seconds elapsed since client started trying to boot."
+            :ACCESSOR SECS :INITARG :SECS)
            (FLAGS :DOCUMENTATION "Flags (see figure 2)." :ACCESSOR FLAGS
-                  :INITARG :FLAGS)
+            :INITARG :FLAGS)
            (CIADDR :DOCUMENTATION
-                   "Client IP address; filled in by client in DHCPREQUEST if verifying previously allocated configuration parameters."
-                   :ACCESSOR CIADDR :INITARG :CIADDR)
+            "Client IP address; filled in by client in DHCPREQUEST if verifying previously allocated configuration parameters."
+            :ACCESSOR CIADDR :INITARG :CIADDR)
            (YIADDR :DOCUMENTATION "'your' (client) IP address." :ACCESSOR
-                   YIADDR :INITARG :YIADDR)
+            YIADDR :INITARG :YIADDR)
            (SIADDR :DOCUMENTATION
-                   "IP address of next server to use in bootstrap; returned in DHCPOFFER, DHCPACK and DHCPNAK by server."
-                   :ACCESSOR SIADDR :INITARG :SIADDR)
+            "IP address of next server to use in bootstrap; returned in DHCPOFFER, DHCPACK and DHCPNAK by server."
+            :ACCESSOR SIADDR :INITARG :SIADDR)
            (GIADDR :DOCUMENTATION
-                   "Relay agent IP address, used in booting via a relay-agent."
-                   :ACCESSOR GIADDR :INITARG :GIADDR)
+            "Relay agent IP address, used in booting via a relay-agent."
+            :ACCESSOR GIADDR :INITARG :GIADDR)
            (CHADDR :DOCUMENTATION "Client hardware address." :ACCESSOR CHADDR
-                   :INITARG :CHADDR)
+            :INITARG :CHADDR)
            (SNAME :DOCUMENTATION
-                  "Optional server host name, null terminated string."
-                  :ACCESSOR SNAME :INITARG :SNAME)
+            "Optional server host name, null terminated string." :ACCESSOR
+            SNAME :INITARG :SNAME)
            (FILE :DOCUMENTATION
-                 "Boot file name, null terminated string; \"generic\" name or null in DHCPDISCOVER, fully qualified directory-path name in DHCPOFFER."
-                 :ACCESSOR FILE :INITARG :FILE)
+            "Boot file name, null terminated string; \"generic\" name or null in DHCPDISCOVER, fully qualified directory-path name in DHCPOFFER."
+            :ACCESSOR FILE :INITARG :FILE)
            (MCOOKIE :DOCUMENTATION "0x63825363" :ACCESSOR MCOOKIE :INITARG
-                    :MCOOKIE)
+            :MCOOKIE)
            (OPTIONS :DOCUMENTATION
-                    "Optional parameters field.  See the options documents for a list of defined options."
-                    :ACCESSOR OPTIONS :INITARG :OPTIONS)))
+            "Optional parameters field.  See the options documents for a list of defined options."
+            :ACCESSOR OPTIONS :INITARG :OPTIONS)))
--- parse-dhcp-options.lisp
+++ parse-dhcp-options.lisp
@@ -10,26 +10,23 @@
     ((list* 255 _)
      '())
     ((list* 12 n rest) ;; hostname option
-     (let ((hn (subseq rest 0 n)))
+     (let ((hn (map 'string #'code-char (subseq rest 0 n))))
        (when debug (format t "hostname=~a~%" hn))
        (parse-options (subseq rest n) :debug debug)))
-
     ((list* 50 4 a b c d rest)
      (let ((req-ip (vector a b c d)))
        (when debug (format t "request ip address=~a~%" req-ip))
        (parse-options rest :debug debug)))
-    
-    ((list* 54 4 a b c d rest)
-     (let ((server (vector a b c d)))
-       (when debug (format t "server-identifier: ~a ~%" server))
-       (parse-options rest :debug debug)))
-    
     ((list* 53 1 3 rest) ;; dhcp discover
      (when debug (format t "dhcp-message-type discover~%"))
      (parse-options rest :debug debug)
      )
+    ((list* 54 4 a b c d rest)
+     (let ((server (vector a b c d)))
+       (when debug (format t "server-identifier: ~a ~%" server))
+       (parse-options rest :debug debug)))
     ((list* 55 n rest) ;; dhcp discover
-     (when debug (format t "parameter requests~%"))
+     (when debug (format t "dhcp discover requests~%"))
      (loop
 	:for i below n
 	:for (a b) on rest by #'cddr
