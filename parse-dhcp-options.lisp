eebae7939decda3657c4af4499fb703d70dff7ec
Eric Gustafson <heapsmash@yandex.com>
2019-05-18 21:45:11 -0400

    updates to parse android moto dhcp request packet

--- parse-dhcp-options.lisp
+++ parse-dhcp-options.lisp
@@ -17,8 +17,12 @@
      (let ((req-ip (vector a b c d)))
        (when debug (format t "request ip address=~a~%" req-ip))
        (parse-options rest :debug debug)))
-    ((list* 53 1 3 rest) ;; dhcp discover
+    ((list* 53 1 1 rest) ;; dhcp discover
      (when debug (format t "dhcp-message-type discover~%"))
+     (parse-options rest :debug debug)
+     )
+    ((list* 53 1 3 rest) ;; dhcp request
+     (when debug (format t "dhcp-message-type request~%"))
      (parse-options rest :debug debug)
      )
     ((list* 54 4 a b c d rest)
@@ -33,6 +37,26 @@
 	:collect (nums-and-txt:octets->num (vector a b)))
      (parse-options (subseq rest (* 2 n)) :debug debug)
      )
+    ((list* 57 2 l1 l2 rest)
+     (let ((num (nums-and-txt:octets->num (list l1 l2))))
+       (when debug (format t "dhcp max message size:~a~%" num))
+       (parse-options rest :debug debug)))
+    ((list* 60 n rest) ;; vendor class
+     (let ((payload (subseq rest 0 n)))
+       (when debug (format t "vendor class identifier:~a~%" (map 'string #'code-char payload))))
+     (parse-options (subseq rest n) :debug debug))
+
+    ((list* 61 n rest) ;; client-identifier
+     (let ((payload (subseq rest 0 n)))
+       (trivia:match
+	   payload
+	 ((list* type clazz-info)
+	  (when debug (format t "client identifier type=~a,~a~%" type clazz-info)))
+	 (otherwise
+	  (when debug (format t "error parsing client-identifier payload ~a" payload))))
+       (parse-options (subseq rest n) :debug debug))
+     )
+     
     ((list* 83 n rest)
      (when debug (format t "rfc4174 - iSNS,n=~a,len(seq)=~a" n (length rest)))
      (let ((after (subseq rest n)))
